6b1108759a867005f53077efe65a689b4d303bc2
yingliu928 <yingliu928@gmail.com>
2021-06-08 23:53:36 -0400

    added

---
+++ findAllPath.py
@@ -0,0 +1,10 @@
+from collections import deque
+import PariInt
+class Solution:
+    def uniquePaths(self,m:int,n:int)-> int:
+        if m == 1 or n == 1:
+            return 1
+
+        return self.uniquePaths(m - 1,n) + self.uniquePaths(m,n-1)
+
+
